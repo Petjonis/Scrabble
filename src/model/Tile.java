4eb104e7349d8f6b28f0638511f0a4c96e9f8f4a
Fabian Petek <fpetek@mail.uni-mannheim.de>
2021-04-09 16:59:34 +0200

    #16 updated createClassicTiles method
    Signed-off-by: Fabian Petek <fpetek@mail.uni-mannheim.de>

--- src/model/Tile.java
+++ src/model/Tile.java
@@ -35,84 +35,111 @@ public class Tile {
     int count = 0;
     while (count < 12) {
       newTiles[count] = new Tile('E', 1, TileType.LETTER);
+      count++;
     }
     while (count < 21) {
       newTiles[count] = new Tile('A', 1, TileType.LETTER);
+      count++;
     }
     while (count < 30) {
       newTiles[count] = new Tile('I', 1, TileType.LETTER);
+      count++;
     }
     while (count < 38) {
       newTiles[count] = new Tile('O', 1, TileType.LETTER);
+      count++;
     }
     while (count < 44) {
       newTiles[count] = new Tile('N', 1, TileType.LETTER);
+      count++;
     }
     while (count < 50) {
       newTiles[count] = new Tile('R', 1, TileType.LETTER);
+      count++;
     }
     while (count < 56) {
       newTiles[count] = new Tile('T', 1, TileType.LETTER);
+      count++;
     }
     while (count < 60) {
       newTiles[count] = new Tile('L', 1, TileType.LETTER);
+      count++;
     }
     while (count < 64) {
       newTiles[count] = new Tile('S', 1, TileType.LETTER);
+      count++;
     }
     while (count < 68) {
       newTiles[count] = new Tile('U', 1, TileType.LETTER);
+      count++;
     }
     while (count < 72) {
       newTiles[count] = new Tile('D', 2, TileType.LETTER);
+      count++;
     }
     while (count < 75) {
       newTiles[count] = new Tile('G', 2, TileType.LETTER);
+      count++;
     }
     while (count < 77) {
       newTiles[count] = new Tile('B', 3, TileType.LETTER);
+      count++;
     }
     while (count < 79) {
       newTiles[count] = new Tile('C', 3, TileType.LETTER);
+      count++;
     }
     while (count < 81) {
       newTiles[count] = new Tile('M', 3, TileType.LETTER);
+      count++;
     }
     while (count < 83) {
       newTiles[count] = new Tile('P', 3, TileType.LETTER);
+      count++;
     }
     while (count < 85) {
       newTiles[count] = new Tile('F', 4, TileType.LETTER);
+      count++;
     }
     while (count < 87) {
       newTiles[count] = new Tile('H', 4, TileType.LETTER);
+      count++;
     }
     while (count < 89) {
       newTiles[count] = new Tile('V', 4, TileType.LETTER);
+      count++;
     }
     while (count < 91) {
       newTiles[count] = new Tile('W', 4, TileType.LETTER);
+      count++;
     }
     while (count < 93) {
       newTiles[count] = new Tile('Y', 4, TileType.LETTER);
+      count++;
     }
     while (count < 95) {
       newTiles[count] = new Tile('K', 5, TileType.LETTER);
+      count++;
     }
     while (count < 96) {
       newTiles[count] = new Tile('J', 8, TileType.LETTER);
+      count++;
     }
     while (count < 97) {
       newTiles[count] = new Tile('X', 8, TileType.LETTER);
+      count++;
     }
     while (count < 98) {
       newTiles[count] = new Tile('Q', 10, TileType.LETTER);
+      count++;
     }
     while (count < 99) {
       newTiles[count] = new Tile('Z', 10, TileType.LETTER);
+      count++;
     }
     while (count <= 100) {
       newTiles[count] = new Tile(' ', 10, TileType.BLANK);
+      count++;
     }
 
     return newTiles;
