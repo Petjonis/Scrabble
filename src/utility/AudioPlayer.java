0a173cbde8b866dddd15d8b1bfa29da6374f1f84
Fabian Petek <fpetek@mail.uni-mannheim.de>
2021-04-10 16:56:41 +0200

    #21 Update 1
    Signed-off-by: Fabian Petek <fpetek@mail.uni-mannheim.de>

--- src/utility/AudioPlayer.java
+++ src/utility/AudioPlayer.java
@@ -1,37 +1,56 @@
 package utility;
 
+import settings.GlobalSettings;
+
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 /**
- * Class to play 16bit .wav audio files
+ * Class to load, play and change volume of 16 byte .wav audio files
  */
 public class AudioPlayer implements Runnable{
-    // System independent path to resource folder
-    private static final String path =
-            System.getProperty("user.dir")
-                    + System.getProperty("file.separator")
-                    + "resources"
-                    + System.getProperty("file.separator");
+    public Clip audio;
 
     /**
      * Method to play a sound.
-     * Thread.sleep() method is needed so audio won't kill itself
-     * @param audioFile is name of audio file.
+     * Thread.sleep() method is needed so audio won't kill itself.
      */
-    public void playAudio(String audioFile){
-        File audioFilePath = new File(path+audioFile);
+    public void playAudio(){
         try {
-            AudioInputStream input = AudioSystem.getAudioInputStream(audioFilePath);
-            Clip audio = AudioSystem.getClip();
-            audio.open(input);
             audio.start();
             Thread.sleep(audio.getMicrosecondLength() / 1000);
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to load an audio file into class variable.
+     *
+     * @param audioFile is name of file, has to be .wav
+     */
+    public void loadAudio(String audioFile){
+        File audioFilePath = new File(GlobalSettings.filepath +audioFile);
+        try {
+            AudioInputStream input = AudioSystem.getAudioInputStream(audioFilePath);
+            audio = AudioSystem.getClip();
+            audio.open(input);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Method to set volume of audio clip
+     *
+     * @param level has to be between 6 and -80, otherwise it throws exception
+     */
+    public void setVolume(int level){
+        FloatControl volume = (FloatControl) this.audio.getControl(FloatControl.Type.MASTER_GAIN);
+        volume.setValue((float) level);
     }
 
     @Override
