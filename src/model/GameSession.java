09689b454b9bbca00c5d33dd1f5830aebde1d302
Sontek Daniel Cho <socho@mail.uni-mannheim.de>
2021-05-03 17:57:18 +0200

    #23 "GameSession" and "GameState" added into model

---
+++ src/model/GameSession.java
@@ -0,0 +1,43 @@
+package model;
+
+import controller.PlayOnlineController;
+import java.util.ArrayList;
+import network.Server;
+
+public class GameSession {
+  private static int gameLobbyId = 0 ;
+  private String gameMode ;
+  private GameState state ;
+  private ArrayList<String> players ;
+  private int port ;
+  private Server server;
+  PlayOnlineController poC1 = new PlayOnlineController();
+
+  public GameSession(){
+    gameLobbyId++;
+    this.players = getPlayers();
+    this.state = GameState.WAITING_LOBBY;
+  }
+
+  public void setState(GameState gs){
+    this.state = gs;
+  }
+
+  public GameState getState(){
+    return this.state;
+  }
+
+  public void setGameMode (String s){
+    this.gameMode = s ;
+  }
+  public String getGameMode(){
+    return this.gameMode;
+  }
+
+  public ArrayList getPlayers(){
+    this.players = new ArrayList<String>(server.getClientNames());
+  return players;
+  }
+
+
+}
---
+++ src/model/GameState.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * this enum represents different state the game session.
+ *
+ * @author socho
+ */
+public enum GameState {
+  WAITING_LOBBY, INGAME, CLOSED;
+}
