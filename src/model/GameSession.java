4733cedc643409c2100bcf7899971a2991c9666b
Sontek Daniel Cho <socho@mail.uni-mannheim.de>
2021-05-04 17:10:43 +0200

    #25 formatting update "GameSession"

--- src/model/GameSession.java
+++ src/model/GameSession.java
@@ -5,38 +5,40 @@ import java.util.ArrayList;
 import network.Server;
 
 public class GameSession {
-  private static int gameLobbyId = 0 ;
-  private String gameMode ;
-  private GameState state ;
-  private ArrayList<String> players ;
-  private int port ;
+
+  private static int gameLobbyId = 0;
+  private String gameMode;
+  private GameState state;
+  private ArrayList<String> players;
+  private int port;
   private Server server;
   PlayOnlineController poC1 = new PlayOnlineController();
 
-  public GameSession(){
+  public GameSession() {
     gameLobbyId++;
     this.players = getPlayers();
     this.state = GameState.WAITING_LOBBY;
   }
 
-  public void setState(GameState gs){
+  public void setState(GameState gs) {
     this.state = gs;
   }
 
-  public GameState getState(){
+  public GameState getState() {
     return this.state;
   }
 
-  public void setGameMode (String s){
-    this.gameMode = s ;
+  public void setGameMode(String s) {
+    this.gameMode = s;
   }
-  public String getGameMode(){
+
+  public String getGameMode() {
     return this.gameMode;
   }
 
-  public ArrayList getPlayers(){
+  public ArrayList getPlayers() {
     this.players = new ArrayList<String>(server.getClientNames());
-  return players;
+    return players;
   }
 
 
