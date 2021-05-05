f02d3c1a0f06cb376a74eddf912c9d01bba9a037
Sontek Daniel Cho <socho@mail.uni-mannheim.de>
2021-05-05 22:47:41 +0200

    #31 "GameInfoController": unfinished updates, still many bugs and misfunction + comments.

--- src/controller/GameInfoController.java
+++ src/controller/GameInfoController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 import com.sun.tools.javac.Main;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,9 +12,11 @@ import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import messages.SendChatMessage;
 import model.GameState;
 
 public class GameInfoController implements Initializable {
+  static GameInfoController gameInfoController;
 
   @FXML
   private AnchorPane gameInfoPane;
@@ -34,31 +37,45 @@ public class GameInfoController implements Initializable {
   private JFXButton sendButton;
 
   @FXML
-  void send(ActionEvent event) {
+  void send(ActionEvent event) throws IOException {
     //demo
-    chatList.getItems().add("Player2: " + sendText.getText());
+   /** problems with "sendTo" method in Network.ServerProtocol.
+    * @author socho
+    * chatList.getItems().add(MainController.mainController.getUserName() + ": " + sendText.getText());
+    MainController.mainController.getConnection()
+        .sendToServer(new SendChatMessage(MainController.mainController
+            .getUserName(), sendText.getText())); */
+    chatList.getItems().add("Player1: Hey!");
     sendText.clear();
   }
 
   //populate lists with demo strings
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    /** TableView initialize with all players, but not 100% functional, only host sees himself and player who joins see himself either.
+    gameInfoController = this;
+    /**
+     * TableView initialize with all players, but not 100% functional, only host sees himself and player who joins see himself either.
+     * @author socho
      * while (PlayOnlineController.playOnlineController.getGameSession().getState() == GameState.WAITING_LOBBY) {
-    if (GameBoardController.gameBoardController.getPlayButton().isPressed()){
-      PlayOnlineController.playOnlineController.getGameSession().setState(GameState.INGAME);
-    }
-  }*/
+     if (GameBoardController.gameBoardController.getPlayButton().isPressed()){
+     PlayOnlineController.playOnlineController.getGameSession().setState(GameState.INGAME);
+     }
+     }*/
 
     for (String player : MainController.mainController.getPlayerList()) {
       playerList.getItems().add(player);
     }
     /**playerList.getItems().add("Player1");
-    playerList.getItems().add("Player2");
-    playerList.getItems().add("Player3");
-    playerList.getItems().add("Player4"); */
+     playerList.getItems().add("Player2");
+     playerList.getItems().add("Player3");
+     playerList.getItems().add("Player4"); */
 
     chatList.getItems().add("Player1: Hello");
 
-
   }
+
+  /** getter method. */
+  public JFXListView<String> getChatList(){
+    return this.chatList;
+  }
+
 }
