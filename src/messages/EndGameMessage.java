c95061e0ded9939c7939967d35021c27107614a4
Fabian Petek <fpetek@mail.uni-mannheim.de>
2021-05-26 12:00:01 +0200

    #32 deleted ResultPlayerListMessage.java and updated EndGameMessage.java to send a Playerlist

--- src/messages/EndGameMessage.java
+++ src/messages/EndGameMessage.java
@@ -2,16 +2,24 @@ package messages;
 
 import model.Player;
 
+import java.util.ArrayList;
+
 public class EndGameMessage extends Message {
 
   private static final long serialVersionUID = 1L;
+  private ArrayList<Player> players;
 
   /**
    * constructor for message.
    *
    * @param player
    */
-  public EndGameMessage(Player player) {
+  public EndGameMessage(Player player, ArrayList<Player> playerList) {
     super(MessageType.END_GAME, player);
+    players = playerList;
+  }
+
+  public ArrayList<Player> getPlayers(){
+        return players;
   }
 }
